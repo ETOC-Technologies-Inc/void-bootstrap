0fee4e6f49f99c3ee0fa9284e2646d47854fd6c0
Oleh Vorobiov <oleh.vorobiov@hobovrlabs.org>
2023-10-08 02:37:08 +0300

    all: Added config.docker_base.sh
    Added a config for base docker images
    that we will be using.
    
    Signed-off-by: Oleh Vorobiov <oleh.vorobiov@hobovrlabs.org>

---
+++ config.docker_base.sh
@@ -0,0 +1,31 @@
+. config.base.sh
+
+arch="x86_64"
+
+musl=false
+
+img_size="3G"
+
+img_name_format="nafoarmories-%a-base-rootfs-$(date +'%Y-%m-%d--%H-%M').img"
+
+extra_void_repos=(
+	nonfree multilib multilib-nonfree
+)
+
+base_pkgs+=(
+	# dejavu-fonts-ttf # for proper font rendering in ff :)
+	# noto-fonts-emoji # for emoji keyboard on gnome shell mobile
+	# Vulkan-Tools glxinfo # for vkcube and other GPU tools
+	# waydroid # for running Android apps
+	# flatpak
+	mariadb mariadb-client python3 python3-mysqlclient
+)
+# extra_build_pkgs+=(
+# 	# libphonenumber openimageio openvdb openimageio blender
+# 	# mutter gnome-shell # gnome shell mobile
+# 	# pmos-tweaks
+# 	# megapixels callaudiod feedbackd #gnome-calls purism-chatty
+# )
+enable_sv+=(
+	mysqld
+)
